5fa999cb6205d871f4bb60eb61083918accada66
Tommy Truong <imtommytr@gmail.com>
2020-08-22 13:24:57 -0700

    Update README and Add Print, Add, and Delete Functionality
    Implemented the option to add new user data to file, select and delete specific data from file, and print all user data to console.
    
    Also updated README to reveal all currently functional options in the program.

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Sign-In Data Saver
+**[NOT FINISHED]**
 Sign-In Data Saver is a python program that allows users to add, edit, and save sign-in information to a JSON file. Users can easily edit sign-in information for many different websites/applications and save the information to the data file.
 
 **Note: In the event that the data file is _deleted/corrupted_, the program will _create/overwrite_ the previous file, which may result in _losing file data_ if the file was corrupted or unreadable.**
 
 **Security Note:**
-**Since the JSON file is _only_ saved on your computer and _not_ sent to any online server/database, your information _isn't_ accessible/editable by others _unless_ your computer has a _virus_ or your physical _hard drive/ssd_ with the JSON file was _stolen_ from your computer.**
 
-**Also, the JSON file is currently _not_ encrypted by the program. So, your information _is_ readable to others if they open the JSON file on your personal computer.**
+**Since the JSON file is _only_ saved on your computer and _not_ sent to any online server/database, your information _isn't_ accessible/editable by anyone else _unless_ your computer has some kind of _virus_ or your physical _hard drive/ssd_ that stored the JSON file was _stolen_ from your computer.**
+
+**Also, the JSON file is currently _not_ encrypted by the program. So, your information _is_ readable to others if they open the JSON file on your personal computer. This will be changed in the final version of the program. The program currently doesn't have a password feature as well, which would only allow the user to see the saved information if the correct password was entered.**
+
+## Add New User Info
+Users can add new sign-in information into the JSON file using the program with ease. In the main menu, users enter the number 2, then enter the neccessary sign-in information and the website/application that uses that sign-in information. Then, the program automatically formats the new data and appends it to the JSON file to be saved for later use.
+
+## Select and Delete User Info
+Users can also easily delete information from the JSON file using the program. Users enter the number 3 in the main menu, then select and confirm the deletion of sign-in information for a specific website/application. Then, the program permanently removes the selected information from the file.
+
+## Print User Info to Console
+In order to see all of the information in the JSON file at once, users can print all of the saved sign-in information to the console. When all of the security features are implemented, the JSON file will be completely unreadable unless it is decrypted by the program. The program will also be inaccessible unless the user enters their password in order to access their information.
--- main.py
+++ main.py
@@ -67,6 +67,8 @@ def invalid_file():
 
 #Checks for JSON file, and creates new file if one doesn't exist/isn't accessible
 def check_for_file():
+  clear_console()
+  
   print("Searching for data file...\n\n")
   sleep(0.7)
 
@@ -91,21 +93,229 @@ def check_for_file():
   clear_console()
 
 
+#Generates new key for new value in JSON dict, then stores used keys in array/list
+def get_new_key():
+  with open("sidsData.json") as used_key_file:
+    file_data = json.load(used_key_file)
+
+  new_key = 1
+
+  while str(new_key) in file_data["used_keys"]:
+    new_key += 1
+
+  with open('sidsData.json', 'w') as used_key_file:
+    file_data["used_keys"].append(str(new_key))
+    
+    json.dump(file_data, used_key_file, indent = 2)
+
+  return new_key
+
+
+#Allows user to select the data they want to modify
+def select_data(modify):
+  print_saved_data()
+
+  selected_data = input("\nSelect the data you want to " + modify + " (Enter 'cancel' to exit this screen):\n>>>> ")
+
+  if selected_data.lower() == 'cancel':
+    sleep(0.7)
+    
+    clear_console()
+
+    check_user_input()
+
+  with open("sidsData.json") as json_file:
+    file_data = json.load(json_file)
+
+  if selected_data in file_data["used_keys"]:
+    confirm = input("\nAre you sure you want to " + modify + ":\n\n" + selected_data + ". Website : " + file_data["user_data"][selected_data]["website"] + "\nEmail : " + file_data["user_data"][selected_data]["email"] +"\nPassword : " + file_data["user_data"][selected_data]["password"] +"\nUsername : " + file_data["user_data"][selected_data]["username"] + "\nPhone Number : " + file_data["user_data"][selected_data]["phone_num"] + "\n\n(Yes or No)\n>>>> ")
+
+    while confirm.lower() not in ['yes', 'no']:
+      print("\nThat is not an option.")
+
+      sleep(0.7)
+
+      clear_console()
+
+      confirm = input("\nAre you sure you want to " + modify + ":\n\n" + selected_data + ". Website : " + file_data["user_data"][selected_data]["website"] + "\nEmail : " + file_data["user_data"][selected_data]["email"] +"\nPassword : " + file_data["user_data"][selected_data]["password"] +"\nUsername : " + file_data["user_data"][selected_data]["username"] + "\nPhone Number : " + file_data["user_data"][selected_data]["phone_num"] + "\n\n(Yes or No)\n>>>> ")
+
+    if confirm.lower() == 'yes':
+      return selected_data
+
+    elif confirm.lower() == 'no':
+      select_data()
+
+  else:
+    print("\nThat is not an option.")
+
+    select_data()
+
+
+# 1. Prints saved user data in JSON file to console
+def print_saved_data():
+  check_for_file()
+
+  clear_console()
+  
+  with open("sidsData.json") as json_file:
+    file_data = json.load(json_file)
+
+  if len(file_data["user_data"]) == 0:
+    print("Your data file has no saved user data.")
+
+    sleep(0.7)
+
+    print("\n\nIn order to print/change user data in JSON file, you must add user data first.")
+
+    sleep(0.7)
+
+    _ = input("\n\nEnter any key to continue:\n>>>> ")
+
+    clear_console()
+    
+    check_user_input()
+  
+  else:
+    for i in file_data["user_data"]:
+      print(i + ". Website : " + file_data["user_data"][i]["website"])
+      print("Email : " + file_data["user_data"][i]["email"])
+      print("Password : " + file_data["user_data"][i]["password"])
+      print("Username : " + file_data["user_data"][i]["username"])
+      print("Phone Number : " + file_data["user_data"][i]["phone_num"] + "\n")
+
+  sleep(0.7)
+
+
+# 2. Adds new user data to JSON file
+def add_new_data():
+  clear_console()
+  
+  check_for_file()
+  
+  website = input("Enter the WEBSITE/APPLICATION that will use this data (Enter 'cancel' to exit data creation screen):\n\n>>>> ")
+
+  if website.lower() == 'cancel':
+    clear_console()
+    
+    check_user_input()
+  
+  email = input("\n\nEnter the EMAIL that is used by the website (Enter 'cancel' to exit data creation screen):\n\n>>>> ")
+
+  if email.lower() == 'cancel':
+    clear_console()
+    
+    check_user_input()
+  
+  password = input("\n\nEnter the PASSWORD that is used by the website (Enter 'cancel' to exit data creation screen):\n\n>>>> ")
+
+  if password.lower() == 'cancel':
+    clear_console()
+    
+    check_user_input()
+  
+  username = input("\n\nEnter the USERNAME that is used by the website (Enter 'cancel' to exit data creation screen; if there's NO username, enter 'n/a'):\n\n>>>> ")
+  if username.lower() == 'n/a':
+    username = 'N/A'
+
+  if username.lower() == 'cancel':
+    clear_console()
+    
+    check_user_input()
+
+  phone_num = input("\n\nEnter the PHONE NUMBER that is used by the website (Enter 'cancel' to exit data creation screen; if there's NO phone number, enter 'n/a'):\n\n>>>> ")
+  
+  if phone_num.lower() == 'n/a':
+    phone_num = 'N/A'
+
+  if phone_num.lower() == 'cancel':
+    clear_console()
+    
+    check_user_input()
+  
+  dict_key = get_new_key()
+
+  new_dict = {dict_key : {
+    'website' : website.title(),
+    'email' : email,
+    'password' : password,
+    'username' : username,
+    'phone_num' : phone_num
+    }
+  }
+
+  with open('sidsData.json', 'r+') as json_file:
+    file_data = json.load(json_file)
+
+    file_data["user_data"].update(new_dict)
+  
+    json_file.seek(0)
+
+    json.dump(file_data, json_file, indent = 2)
+
+  sleep(0.7)
+  
+  print("\n\nNew saved data added.")
+
+
+
+# 3. Deletes selected user data from JSON file
+def delete_data():
+  selected_data = select_data('delete')
+
+  with open("sidsData.json") as json_file:
+    file_data = json.load(json_file)
+  
+  file_data["used_keys"].remove(selected_data)
+
+  del file_data["user_data"][selected_data]
+      
+  with open("sidsData.json", "w") as json_file:
+    json.dump(file_data, json_file, indent = 2)
+
+  sleep(0.7)
+  
+  print("\n\nSelected data was successfully deleted.")
+
+
+#[NOT FINISHED IMPLEMENTING]
+# 4. Edits selected data in JSON file
+def edit_data():
+  selected_data = select_data('edit')
+
+
 #Checks user input, then calls corresponding functions
 def check_user_input():
   choice = get_input()
   
   if choice == '1':
-    print("\n\nUser chose option 1.")
+    print_saved_data()
+
+    _ = input("\nEnter any key to continue:\n>>>> ")
+
+    clear_console()
+
+    check_user_input()
   
   elif choice == '2':
-    print("\n\nUser chose option 2.")
+    add_new_data()
+
+    clear_console()
+
+    check_user_input()
   
   elif choice == '3':
-    print("\n\nUser chose option 3.")
+    delete_data()
+
+    clear_console()
+
+    check_user_input()
 
   elif choice == '4':
-    print("\n\nUser chose option 4.")
+    edit_data()
+
+    clear_console()
+
+    check_user_input()
   
   elif choice == '5':
     sleep(0.7)
